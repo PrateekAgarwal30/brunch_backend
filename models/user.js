ff6fd0d14e37251baf2b47223092306471a59d7f
Prateek Agarwal <prateek.agarwal.30@gmail.com>
2019-02-02 00:45:21 +0530

    Added user profile detail api and corrected model schema

--- models/user.js
+++ models/user.js
@@ -19,7 +19,8 @@ const userSchema = new mongoose.Schema({
     },
     details : {
         type : mongoose.Types.ObjectId,
-        required : true
+        required : true,
+        ref : 'details'
     }
 });
 userSchema.methods.generateAuthToken = function(){
--- start_up/routes.js
+++ start_up/routes.js
@@ -1,8 +1,10 @@
 const register = require('../routes/register');
 const auth = require('../routes/auth');
+const me = require("../routes/me")
 const express = require('express');
 module.exports = function routes(app){ 
     app.use('/api/register',register);
     app.use('/api/auth', auth);
+    app.use("/api/me", me);
     console.log('Routes Added');
 }
