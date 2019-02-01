5801083daf87fa041f6e41db68d49845fa552a5b
Prateek Agarwal <Prateek.Agarwal.30@gmail.com>
2019-02-02 00:45:21 +0530

    Added user profile detail api and corrected model schema

---
+++ routes/me.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { User } = require('../models/user')
+router.get('/', async (req, res) => {
+    const jwtToken = req.get('x-auth-token')
+    try {
+        let user = jwt.verify(jwtToken, config.get('jwtPrivateKey'));
+        user = await User.findById(user._id).populate('details',"-_id");
+        res.status(200).send({
+            _status: "success",
+            _data: user
+        });
+    } catch (ex) {
+        res.status(400).send({
+            _status: "fail",
+            _message: ex.message
+        });
+    }
+});
+module.exports = router;
