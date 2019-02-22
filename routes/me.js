13cf9c924f2abdb84aa97eb98b920b5998613c77
Prateek Agarwal <prateek.agarwal.30@gmail.com>
2019-02-22 20:23:22 +0530

    General : Updated Add Addresses API + Code Cleaning

--- routes/me.js
+++ routes/me.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const config = require('config');
 const { User } = require('../models/user');
 const { Detail, validateUpdateDetails } = require('../models/detail');
 const auth = require('../middleware/auth')
@@ -43,36 +41,36 @@ router.post('/details', auth, async (req, res) => {
             });
         };
         let user = await User.findById(req.userId).select("details");
-        if(user.details){
+        if (user.details) {
             let details = await Detail.findById(user.details);
-        if(mDetails.firstName){
-            details.firstName = mDetails.firstName
+            if (mDetails.firstName) {
+                details.firstName = mDetails.firstName
+            }
+            if (mDetails.lastName) {
+                details.lastName = mDetails.lastName
+            }
+            if (mDetails.phoneNumber) {
+                details.phoneNumber = mDetails.phoneNumber
+            }
+            if (mDetails.location) {
+                details.location = mDetails.location
+            }
+            if (mDetails.dateOfBirth) {
+                details.dateOfBirth = mDetails.dateOfBirth
+            }
+            if (mDetails.preferredMeal) {
+                details.preferredMeal = mDetails.preferredMeal
+            }
+            if (mDetails.description) {
+                details.description = mDetails.description
+            }
+            console.log(details);
+            await details.save();
+            res.status(200).send({
+                _status: "success",
+                _data: { details }
+            });
         }
-        if (mDetails.lastName) {
-            details.lastName = mDetails.lastName
-        }
-        if (mDetails.phoneNumber) {
-            details.phoneNumber = mDetails.phoneNumber
-        }
-        if (mDetails.location) {
-            details.location = mDetails.location
-        }
-        if (mDetails.dateOfBirth) {
-            details.dateOfBirth = mDetails.dateOfBirth
-        }
-        if (mDetails.preferredMeal) {
-            details.preferredMeal = mDetails.preferredMeal
-        }
-        if (mDetails.description) {
-            details.description = mDetails.description
-        }
-        console.log(details);
-        await details.save();
-        res.status(200).send({
-            _status: "success",
-            _data: { details }
-        });
-    }
     } catch (ex) {
         res.status(400).send({
             _status: "fail",
