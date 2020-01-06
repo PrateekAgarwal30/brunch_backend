a3226427292f1ff162ccb29efcc72e0aef33b570
Prateek Agarwal <prateek.agarwal.30@gmail.com>
2020-01-07 01:04:47 +0530

    Restructuring App (Separating routes and controllers) for better App Management

---
+++ controllers/addressController.js
@@ -0,0 +1,60 @@
+const { Address, validateAddress } = require("../models/addresses");
+const { User } = require("../models/user");
+const _ = require("lodash");
+const Fawn = require("fawn");
+const postAddress = async (req, res) => {
+  try {
+    console.log("Post - Address");
+    const { error } = validateAddress(
+      _.pick(req.body, ["tech_park_id", "stall_loc_id"])
+    );
+    if (error) {
+      return res.status(400).send({
+        _status: "fail",
+        _message: error.details[0].message
+      });
+    }
+    let user = await User.findById(req.userId, "-__v -password");
+    if (!user) {
+      return res.status(400).send({
+        _status: "fail",
+        _message: "Invalid User"
+      });
+    }
+    const newAddress = new Address(
+      _.pick(req.body, ["tech_park_id", "stall_loc_id"])
+    );
+    user.addresses = newAddress._id;
+    await Fawn.Task()
+      .save("addresses", newAddress)
+      .update("users", { _id: user._id }, { addresses: user.addresses })
+      .run();
+    res.status(200).send({
+      _status: "success",
+      _data: { newAddress, user }
+    });
+  } catch (ex) {
+    res.status(400).send({
+      _status: "fail",
+      _message: ex.message
+    });
+  }
+};
+
+const getAddress = async (req, res) => {
+  try {
+    const user = await User.findById(req.userId, "-__v -password")
+      .populate("details", "-_id")
+      .populate("addresses");
+    res.status(200).send({
+      _status: "success",
+      _data: user
+    });
+  } catch (ex) {
+    res.status(400).send({
+      _status: "fail",
+      _message: ex.message
+    });
+  }
+};
+module.exports = { getAddress, postAddress };
--- routes/techAddresses.js
+++ routes/techAddresses.js
@@ -1,105 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
-const { Address, validateAddress,validateLocation,StallLocation} = require("../models/tech_parks");
-const { User } = require("../models/user");
-const _ = require("lodash");
-const Fawn = require("fawn");
-router.get("/", auth, async (req, res) => {
-  try {
-    const addresses = await Address.find().populate(
-      "stall_locations"
-    );
-    res.status(200).send({
-      _status: "success",
-      _data: addresses
-    });
-  } catch (ex) {
-    res.status(400).send({
-      _status: "fail",
-      _message: ex.message
-    });
-  }
-});
-router.post("/", auth, async (req, res) => {
-  try {
-    console.log("Post - Tech Address");
-    let addAddress = _.pick(req.body, [
-      "techPark",
-      "company",
-      "tPark_location",
-      "address",
-      "area",
-      "city",
-    ]);
-    let { error } = validateAddress(
-      addAddress
-    );
-    if (error) {
-      return res.status(400).send({
-        _status: "fail",
-        _message: error.details[0].message
-      });
-    }
-    const newAddress = new Address({ ...addAddress, stall_locations:[]});
-    // await Fawn.Task()
-    //   .save("tech_parks", newAddress)
-    //   .run()
-    await newAddress.save();
-    res.status(200).send({
-      _status: "success",
-      _data: {newAddress}
-    });
-  } catch (ex) {
-    res.status(400).send({
-      _status: "fail",
-      _message: ex.message
-    });
-  }
-});
-
-router.post("/location", auth, async (req, res) => {
-  try {
-    console.log("Post - Stall Location");
-    let addLocation = _.pick(req.body, [
-      "location",
-      "tag",
-    ]);
-    let { error } = validateLocation(
-      addLocation
-    );
-    if (error) {
-      return res.status(400).send({
-        _status: "fail",
-        _message: error.details[0].message
-      });
-    }
-
-    let tech_park = await Address.findById(req.body.tech_park_id);
-    if (!tech_park) {
-      return res.status(400).send({
-        _status: "fail",
-        _message: "Invalid Tech Park Id"
-      });
-    }
-    const newLocation = new StallLocation(_.pick(req.body, [
-      "location",
-      "tag"
-    ]));
-    tech_park.stall_locations.push(newLocation._id);
-    await Fawn.Task()
-      .save("stall_locations", newLocation)
-      .update("tech_parks", { _id: tech_park._id }, { stall_locations: tech_park.stall_locations })
-      .run()
-    res.status(200).send({
-      _status: "success",
-      _data: { tech_park, newLocation }
-    });
-  } catch (ex) {
-    res.status(400).send({
-      _status: "fail",
-      _message: ex.message
-    });
-  }
-});
+const techAddressesController = require("./../controllers/techAddressesController");
+router.get("/", auth, techAddressesController.getTechParks);
+router.post("/", auth, techAddressesController.postNewTechPark);
+router.post(
+  "/location",
+  auth,
+  techAddressesController.postNewStallLocationForTechPark
+);
 module.exports = router;
